826dc210acfc32deed87778f193a77b833576b83
bot-enik <38273715+bot-enik@users.noreply.github.com>
2019-01-17 20:49:42 +0300

    Update 1427.py

--- Sorting/1427.py
+++ Sorting/1427.py
@@ -2,9 +2,11 @@
 # Task 1427
 # This solution is not fully correct
 
+# n - число строк (число списков)
+# m - стобцов (элементов в одиночном списке)
 n, m = [int(i) for i in input().split()]
-# n - число столбцов (x), m - число строк (y)
-A = [[int(i) for i in input().split()] for j in range(m)]
+
+A = [[int(i) for i in input().split()] for j in range(n)]
 
 
 res = 0
@@ -13,16 +15,15 @@ for B in A:
     min_B = min(B)
     c = B.count(min_B)
     # Ищем минимальные элементы в строке
-    for i in range(n):
+    for i in range(m):
         if c == 0:
             break
         if B[i] == min_B:
             # Просмотрим этот столбец(проверка, что макс в нем)
-            flag = True
-            for j in range(m):
+            for j in range(n):
                 if A[j][i] > B[i]:
-                    flag = False
-            if flag:
+                    break
+            else:
                 res += 1
-        c -= 1
+            c -= 1
 print(res)
