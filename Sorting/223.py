2e3bce3c915258539468fba597d738e0cca305b3
bot-enik <38273715+bot-enik@users.noreply.github.com>
2019-01-17 09:10:28 +0300

    Update 223.py

--- Sorting/223.py
+++ Sorting/223.py
@@ -1,3 +1,20 @@
 # https://informatics.msk.ru/mod/statements/view.php?id=270#1
+# Task 223
 
+n = int(input())
 
+A = []
+S = input().split()
+for i in range(n):
+  A += [int(S[i])]
+# Вариант попроще, но без учета количества вводимых чисел:  
+# A = [int(i) for i in input().split()]
+  
+k = int(input())
+count = 0
+
+for i in A:
+    if i == k:
+        count += 1
+
+print(count)
